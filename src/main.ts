45405772f0761ae50534ebd0639368892c66fdd8
Darya Zakhar <daryazakhar.89@gmail.com>
2023-06-05 10:32:32 +0400

    feat: setting work with swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import config from '../config';
+import { serve, setup } from 'swagger-ui-express';
+import { readFileSync } from 'fs';
+import * as YAML from 'yaml';
 
 const { PORT } = config;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const file = readFileSync('doc/api.yaml', 'utf8');
+  const swaggerDocument = YAML.parse(file);
+
+  // Specify the path to your OpenAPI specification file
+  app.use('/doc', serve, setup(swaggerDocument));
 
   await app.listen(PORT);
 }
