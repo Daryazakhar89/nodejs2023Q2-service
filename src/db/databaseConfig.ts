dd1e4020911c9626a90163daa2241ef3d2305a21
Darya Zakhar <daryazakhar.89@gmail.com>
2023-06-19 11:04:43 +0400

    feat: create databaseConfig and new migration

--- src/db/databaseConfig.ts
+++ src/db/databaseConfig.ts
@@ -1,19 +1,30 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSourceOptions } from 'typeorm';
 import config from '../../config';
+import { User } from '../user/user.entity';
+import { Artist } from '../artist/artist.entity';
+import { Favorites } from '../favorites/favorites.entity';
+import { Album } from '../album/album.entity';
+import { Track } from '../track/track.entity';
 
-const { POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_USER } = config;
+const {
+  POSTGRES_DB,
+  POSTGRES_PASSWORD,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+  POSTGRES_HOST,
+} = config;
 
 const databaseConfig: TypeOrmModuleOptions & DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: POSTGRES_HOST,
   port: +POSTGRES_PORT,
   username: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
   database: POSTGRES_DB,
-  entities: ['dist/**/**/*.entity.js'],
-  synchronize: false,
-  migrations: ['dist/**/**/migrations/*.js'],
-  migrationsRun: true,
+  entities: [User, Favorites, Artist, Album, Track],
+  synchronize: true,
+  migrations: [__dirname + '/migrations/*.ts'],
 };
+
 export { databaseConfig };
