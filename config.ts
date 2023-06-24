d9afea642250cd63542e66ea78ce0e856b5e2260
Darya Zakhar <daryazakhar.89@gmail.com>
2023-06-25 01:08:41 +0400

    refactor: update .env, config.ts and package.json

--- config.ts
+++ config.ts
@@ -5,6 +5,7 @@ const config = {
   PORT: process.env.PORT || 4000,
   CRYPT_SALT: process.env.CRYPT_SALT,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  IS_PUBLIC_KEY: process.env.IS_PUBLIC_KEY,
   JWT_SECRET_REFRESH_KEY: process.env.JWT_SECRET_REFRESH_KEY,
   TOKEN_EXPIRE_TIME: process.env.TOKEN_EXPIRE_TIME,
   TOKEN_REFRESH_EXPIRE_TIME: process.env.TOKEN_REFRESH_EXPIRE_TIME,
